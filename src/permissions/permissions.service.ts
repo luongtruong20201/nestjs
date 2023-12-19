12206fc5eabdb7141fff3fec8fcfc552ca661657
luongtruong20201 <luongtruong20201@gmail.com>
2023-12-19 22:42:59 +0700

    ^^

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,26 +1,94 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Permission, PermissionDocument } from './schemas/permission.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class PermissionsService {
-  create(createPermissionDto: CreatePermissionDto) {
-    return 'This action adds a new permission';
+  constructor(
+    @InjectModel(Permission.name)
+    private readonly permissionModel: SoftDeleteModel<PermissionDocument>,
+  ) {}
+
+  async create(createPermissionDto: CreatePermissionDto, user: IUser) {
+    const { _id, email } = user;
+    const { apiPath, method, module, name } = createPermissionDto;
+    const isExist = await this.permissionModel.findOne({ apiPath, method });
+    if (isExist) {
+      throw new BadRequestException(
+        `Permisson với api ${apiPath} và method: ${method} đã tồn tại`,
+      );
+    }
+    const result = await this.permissionModel.create({
+      apiPath,
+      method,
+      module,
+      name,
+      createdBy: { _id: _id, email },
+    });
+
+    return result;
   }
 
-  findAll() {
-    return `This action returns all permissions`;
+  async findAll(current: number, pageSize: number, qs: string) {
+    const { filter, sort, projection, population } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+    const offset = (current - 1) * pageSize;
+    const defaultLimit = pageSize ? pageSize : 10;
+    const totalItems = (await this.permissionModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+    const permissions = await this.permissionModel
+      .find(filter)
+      .sort(sort as any)
+      .skip(offset)
+      .limit(pageSize)
+      .select(projection)
+      .populate(population)
+      .exec();
+    return {
+      meta: {
+        current: current,
+        pageSize: pageSize,
+        pages: totalPages,
+        total: totalItems,
+      },
+      result: permissions,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} permission`;
+  findOne(id: string) {
+    return this.permissionModel.findById(id);
   }
 
-  update(id: number, updatePermissionDto: UpdatePermissionDto) {
-    return `This action updates a #${id} permission`;
+  async update(
+    id: string,
+    updatePermissionDto: UpdatePermissionDto,
+    user: IUser,
+  ) {
+    const result = await this.permissionModel.updateOne(
+      { _id: id },
+      {
+        ...updatePermissionDto,
+        updatedBy: { _id: user._id, email: user.email },
+      },
+    );
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} permission`;
+  async remove(id: string, user: IUser) {
+    const [, result] = await Promise.all([
+      this.permissionModel.updateOne(
+        { _id: id },
+        { deletedBy: { _id: user._id, email: user.email } },
+      ),
+      this.permissionModel.softDelete({ _id: id }),
+    ]);
+
+    return result;
   }
 }
--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,1 +1,19 @@
-export class CreateRoleDto {}
+import { IsArray, IsBoolean, IsMongoId, IsNotEmpty } from 'class-validator';
+import mongoose from 'mongoose';
+
+export class CreateRoleDto {
+  @IsNotEmpty({ message: 'name không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'description không được để trống' })
+  description: string;
+
+  @IsNotEmpty({ message: 'isActive không được để trống' })
+  @IsBoolean({ message: 'isActive có giá trị là boolean' })
+  isActive: boolean;
+
+  @IsNotEmpty({ message: 'permissions không được để trống' })
+  @IsMongoId({ each: true, message: 'mỗi permission phải là mongodb objectId' })
+  @IsArray({ message: 'permissions phải có định dạng là array' })
+  permissions: mongoose.Schema.Types.ObjectId[];
+}
